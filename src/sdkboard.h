501efdb05bbd5806f6f7d80023cafc7f4e7f1927
dilddream <dilddream31@gmail.com>
2025-09-02 20:33:10 +0900

    Add: Implement sdkboard APIs

--- src/sdkboard.h
+++ src/sdkboard.h
@@ -1,23 +1,24 @@
-#ifndef _SDKINIT_H
-#define _SDKINIT_H
+#ifndef _SDKBOARD_H
+#define _SDKBOARD_H
 
 #include <stdio.h>
 #include "sdkset.h"
 
 
-// Sudoku Data-structures
 typedef struct {
 	int board[9][9];
 } sdkboard;
 
 
 // Sudoku Board APIs
-void sudoku_board_create();
-void write_sudoku_cell(int idx1, int idx2, int num, sdkboard *board);
+sdkboard sdkboard_create();
+void write_sdkboard_cell(int idx1, int idx2, int num, sdkboard *board);
 
 
 
-sdkboard sudoku_board_create()
+
+// Sudoku Board Functions
+sdkboard sdkboard_create()
 {
 	sdkboard board = {0,};
 	return board;
@@ -31,4 +32,4 @@ void write_sudoku_cell(int idx1, int idx2, int num, sdkboard *board)
 
 
 
-#endif	// _SDKINIT_H
+#endif	// _SDKBOARD_H
